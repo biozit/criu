70c57e57f7ca11722de170f679c028e0fcd4d76a
Adrian Reber <areber@redhat.com>
2020-09-17 17:05:33 +0000

    scripts: run shellcheck on the scripts folder
    Shellcheck (https://github.com/koalaman/shellcheck) can identify common
    errors in shell scripts. This initial integration of shellcheck only
    checks the scripts in the 'scripts/' folder. This commit fixes (or
    disables) all reports of shellcheck to ensure this part starts error
    free. I am not convinced this is really necessary as most changes do not
    seem to be necessary for their circumstances. On the other hand it
    probably does not hurt to use a checker to avoid unnecessary errors.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- scripts/travis/openj9-test.sh
+++ scripts/travis/openj9-test.sh
@@ -6,14 +6,14 @@ failures=""
 
 docker build -t criu-openj9-ubuntu-test:latest -f scripts/build/Dockerfile.openj9-ubuntu .
 docker run --rm --privileged criu-openj9-ubuntu-test:latest
-if [ $? -ne 0 ]; then
-	failures=`echo "$failures ubuntu"`
+if ! docker run --rm --privileged criu-openj9-ubuntu-test:latest; then
+	failures="$failures ubuntu"
 fi
 
 docker build -t criu-openj9-alpine-test:latest -f scripts/build/Dockerfile.openj9-alpine .
 docker run --rm --privileged criu-openj9-alpine-test:latest
-if [ $? -ne 0 ]; then
-	failures=`echo "$failures alpine"`
+if ! docker run --rm --privileged criu-openj9-alpine-test:latest; then
+	failures="$failures alpine"
 fi
 
 if [ -n "$failures" ]; then
